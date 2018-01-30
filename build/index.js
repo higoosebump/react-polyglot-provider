264f09e8e3158fa233781a7748a6492a8d8b7636
Axel Delafosse <axel.delafosse@epitech.eu>
2018-01-30 15:19:48 +0100

    Update

--- build/index.js
+++ build/index.js
@@ -623,36 +623,40 @@ module.exports = function getPolyfill() {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+exports.default = void 0;
 
 var _react = __webpack_require__(12);
 
-var _propTypes = __webpack_require__(13);
+var _propTypes = _interopRequireDefault(__webpack_require__(13));
 
-var _propTypes2 = _interopRequireDefault(_propTypes);
-
-var _nodePolyglot = __webpack_require__(18);
-
-var _nodePolyglot2 = _interopRequireDefault(_nodePolyglot);
+var _nodePolyglot = _interopRequireDefault(__webpack_require__(18));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var PolyglotProvider = function (_Component) {
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var PolyglotProvider =
+/*#__PURE__*/
+function (_Component) {
   _inherits(PolyglotProvider, _Component);
 
   function PolyglotProvider(props) {
+    var _this;
+
     _classCallCheck(this, PolyglotProvider);
 
-    var _this = _possibleConstructorReturn(this, (PolyglotProvider.__proto__ || Object.getPrototypeOf(PolyglotProvider)).call(this, props));
-
-    _this.polyglot = new _nodePolyglot2.default({
+    _this = _possibleConstructorReturn(this, (PolyglotProvider.__proto__ || Object.getPrototypeOf(PolyglotProvider)).call(this, props));
+    _this.polyglot = new _nodePolyglot.default({
       locale: props.locale,
       phrases: props.wordings[props.locale]
     });
@@ -663,7 +667,7 @@ var PolyglotProvider = function (_Component) {
   }
 
   _createClass(PolyglotProvider, [{
-    key: 'componentWillReceiveProps',
+    key: "componentWillReceiveProps",
     value: function componentWillReceiveProps(newProps) {
       if (newProps.locale !== this.props.locale) {
         this.polyglot.locale(newProps.locale);
@@ -671,7 +675,7 @@ var PolyglotProvider = function (_Component) {
       }
     }
   }, {
-    key: 'render',
+    key: "render",
     value: function render() {
       return this.props.render(this.state);
     }
@@ -681,12 +685,12 @@ var PolyglotProvider = function (_Component) {
 }(_react.Component);
 
 PolyglotProvider.propTypes = {
-  locale: _propTypes2.default.string.isRequired,
-  wordings: _propTypes2.default.object.isRequired,
-  render: _propTypes2.default.func.isRequired
+  locale: _propTypes.default.string.isRequired,
+  wordings: _propTypes.default.object.isRequired,
+  render: _propTypes.default.func.isRequired
 };
-
-exports.default = PolyglotProvider;
+var _default = PolyglotProvider;
+exports.default = _default;
 
 /***/ }),
 /* 12 */
